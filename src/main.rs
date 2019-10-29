c10b9a715d12c221ce6fe78e669dd8f8775f615c
Rutaihwa <rutaihwa@gmail.com>
2019-10-29 18:28:41 +0100

    Start simple server with random byte generations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+use hyper::{Body, Response, Server};
+
 fn main() {
-    println!("Hello, world!");
+    let addr = ([127, 0, 0, 1], 8080).into();
+
+    let builder = Server::bind(&addr);
+
+    let server = builder.serve(|| {
+        service_fn_ok(|_| {
+            let random_byte = rand::random::<u8>();
+            Response::new(Body::from(random_byte.to_string()))
+        })
+    });
+
+    let server = server.map_err(drop);
+    hyper::rt::run(server);
 }

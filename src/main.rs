f225cd6cd2ff5dc75952dbf3084788dd2cfe51f1
Rutaihwa <rutaihwa@gmail.com>
2019-10-30 14:35:56 +0100

    Using environment variables and arguments to the service

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,50 @@
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
 use hyper::rt::Future;
 use hyper::service::service_fn_ok;
 use hyper::{Body, Response, Server};
 use log::{debug, info, trace};
 use pretty_env_logger as logger;
+use std::env;
 
 fn main() {
+    // Argument parser
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(
+            Arg::with_name("address")
+                .short("a")
+                .long("address")
+                .value_name("ADDRESS")
+                .help("Sets an address")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Sets a custom config file")
+                .takes_value(true),
+        )
+        .get_matches();
+
     // Initialize logger
     logger::init();
-    info!("Random microservice - v.0.1.0");
+    info!(" {} - {}", crate_name!(), crate_version!());
 
     trace!("Starting server");
 
-    let addr = ([127, 0, 0, 1], 8080).into();
+    // Server address
+    let addr = matches
+        .value_of("address")
+        .map(|s| s.to_owned())
+        .or(env::var("ADDRESS").ok())
+        .or(dotenv::var("ADDRESS").ok())
+        .unwrap_or_else(|| "127.0.0.1:8080".into())
+        .parse()
+        .expect("Could'nt parse ADDRESS variable");
 
     debug!("Trying to bind server to address: {}", addr);
     let builder = Server::bind(&addr);
